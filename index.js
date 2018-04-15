8101fa5210f7d6d0a4f4f73e41142ef8e2cb9cb2
Justin Tooley <jstooley823@gmail.com>
2018-04-15 17:45:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class BoardMember {
-  
-  constructor( name , homestate , training ){
+
+  constructor( name , homeState , training ){
     this.name = name
     this.homeState = homeState
     this.training = training
